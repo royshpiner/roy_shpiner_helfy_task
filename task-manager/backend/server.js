befa80f94381ce8ad4e4bd7a52ea7acb84e30f0c
Roy <roy.shpiner@post.runi.ac.il>
2026-09-25 14:37:30 +0300

    connected frontend to backend

--- task-manager/backend/server.js
+++ task-manager/backend/server.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
+const cors = require("cors")
 const taskRoutes = require("./routes/tasks");
 
 const app = express();
 const PORT = 4000;
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
