88a0d80821d497db0bdec896f13dcfd05ebaa99c
Roy <roy.shpiner@post.runi.ac.il>
2026-09-25 13:52:59 +0300

    finished backend

--- task-manager/backend/routes/tasks.js
+++ task-manager/backend/routes/tasks.js
@@ -1,5 +1,7 @@
 
 const express = require("express");
+
+const validateTask = require("../middleware/validateTask")
 const router = express.Router();
 
 let tasks = [];   // in memort storage
@@ -10,7 +12,7 @@ router.get("/", (req, res) =>{
 
 //handle post requests
 let nextId = 1;   //id is given by the server and autoincremented
-router.post("/", (req, res) => {
+router.post("/", validateTask,(req, res) => {
     const {title, description, priority} = req.body;
     const newTask ={
         id: nextId++,
@@ -22,5 +24,46 @@ router.post("/", (req, res) => {
     tasks.push(newTask);
     res.status(201).json(newTask);
 })
+
+//handle put requests
+router.put("/:id", validateTask, (req, res) => {
+    const id = Number(req.params.id);
+    const {title, description, completed ,priority} = req.body;
+    const task = tasks.find(task => task.id === id);
+    if(!task){
+        return res.status(404).json({error: "Not found"});
+    }
+    task.title = title;
+    task.description = description;
+    task. completed = completed;
+    task.priority = priority;
+
+    res.status(200).json(task);
+})
+
+
+//handling delete requests
+router.delete("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const taskIndex = tasks.findIndex(task =>task.id === id);
+    if (taskIndex === -1){
+        return res.status(404).json({error: "Not found"});
+    }
+    tasks.splice(taskIndex, 1);
+    res.status(204).send();
+})
+
+//handle patch requests
+router.patch("/:id/toggle", (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find(task =>task.id === id);
+    if (!task){
+        return res.status(404).json({error: "Not found"});
+    }
+    task.completed = !task.completed;
+    res.status(200).json(task);
+})
+
+
 
 module.exports = router;
