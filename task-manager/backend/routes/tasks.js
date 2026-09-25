49cbec2ba6260a975ebffde695505971300519e9
Roy <roy.shpiner@post.runi.ac.il>
2026-09-25 13:15:50 +0300

    added post handling

--- task-manager/backend/routes/tasks.js
+++ task-manager/backend/routes/tasks.js
@@ -2,8 +2,25 @@
 const express = require("express");
 const router = express.Router();
 
-let tasks = {}
-router.get("/", (req, res) =>{
+let tasks = [];   // in memort storage
+// handle get requests
+router.get("/", (req, res) =>{ 
     res.status(200).json(tasks);
 });
+
+//handle post requests
+let nextId = 1;   //id is given by the server and autoincremented
+router.post("/", (req, res) => {
+    const {title, description, priority} = req.body;
+    const newTask ={
+        id: nextId++,
+        title,
+        description,
+        completed: false,
+        priority
+    };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+})
+
 module.exports = router;
